423af3e63afe2cad5785a8ba749b7213e655885f
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-12 22:02:19 +0200

    Finished testing until like 63

--- test/TalentCommunitySale.t.sol
+++ test/TalentCommunitySale.t.sol
@@ -5,6 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {TalentCommunitySale} from "../src/TalentCommunitySale.sol";
 import {USDTMock} from "./ERC20Mock.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {ERC20InsufficientBalance} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
 contract TalentCommunitySaleTest is Test {
     TalentCommunitySale talentCommunitySale;
@@ -86,30 +87,76 @@ contract TalentCommunitySaleTest is Test {
         uint32 tier1MaxBuys = talentCommunitySale.TIER1_MAX_BUYS(); // 100
 
         for (uint256 i = 1; i <= tier1MaxBuys + 1; i++) {
-          address caller = address(uint160(uint256(keccak256(abi.encodePacked(i)))));
-          // Panos address
-          // John address
-          // ... more address
+            address caller = address(uint160(uint256(keccak256(abi.encodePacked(i)))));
+            // Panos address
+            // John address
+            // ... more address
 
-          uint256 amount = 100 * 10 ** tokenDecimals;
+            uint256 amount = 100 * 10 ** tokenDecimals;
+            paymentToken.transfer(caller, amount);
+            // Panos now has 100M USDT
+            // John 100M USDT
+            // ..100 addresses everything goes well.
+
+            // Panos approves the contract TalentCommunitySale to spend that 100M
+            // John approve....
+            vm.prank(caller); // sets the "msg.sender" of the next contract call.
+            paymentToken.approve(address(talentCommunitySale), amount);
+
+            if (i == tier1MaxBuys + 1) { // i is 101
+              vm.expectRevert("TalentCommunitySale: Tier 1 sold out");
+            }
+
+            // Panos is asking the contract TaletnCommunitySale to buyAtTier1 100M
+            // John is asking...
+            vm.prank(caller);
+            talentCommunitySale.buyTier1();
+        }
+    }
+
+    function test_WhenCallerHasAlreadyBought_ItReverts() public {
+        talentCommunitySale.enableSale();
+
+        address caller = address(12347);
+
+        uint256 amount = 100 * 10 ** tokenDecimals;
+
+        for (uint i = 1; i <= 2; i++) {
           paymentToken.transfer(caller, amount);
-          // Panos now has 100M USDT
-          // John 100M USDT
-          // ..100 addresses everything goes well.
 
-          // Panos approves the contract TalentCommunitySale to spend that 100M
-          // John approve....
-          vm.prank(caller); // sets the "msg.sender" of the next contract call.
+          vm.prank(caller);
           paymentToken.approve(address(talentCommunitySale), amount);
 
-          if (i == tier1MaxBuys + 1) { // i is 101
-            vm.expectRevert("TalentCommunitySale: Tier 1 sold out");
+          if (i == 2) {
+            vm.expectRevert("TalentCommunitySale: Address already bought");
           }
-
-          // Panos is asking the contract TaletnCommunitySale to buyAtTier1 100M
-          // John is asking...
           vm.prank(caller);
           talentCommunitySale.buyTier1();
         }
     }
+
+    function test_WhenCallerDoesNotHaveEnoughBalance_ItReverts() public {
+      talentCommunitySale.enableSale();
+
+        address caller = address(12347);
+
+        uint256 amount = 100 * 10 ** tokenDecimals;
+
+        paymentToken.transfer(caller, amount - 1);
+
+        vm.prank(caller);
+        paymentToken.approve(address(talentCommunitySale), amount);
+
+        vm.prank(caller);
+        // vm.expectRevert(abi.encode(
+        //     "ERC20InsufficientBalance",
+        //     caller,            // from address
+        //     amount - 1,       // balance Alice has
+        //     amount        // amount Alice is trying to transfer
+        // ));
+        vm.expectRevert();
+        talentCommunitySale.buyTier1();
+    }
+
+    // TODO: Write tests for lines 65 - 68
 }
