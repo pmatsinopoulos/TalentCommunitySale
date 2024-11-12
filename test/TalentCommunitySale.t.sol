5d714bd64519f0e83984e496ddd845e95bb7be99
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-12 20:17:42 +0200

    Formatting fixes

--- test/TalentCommunitySale.t.sol
+++ test/TalentCommunitySale.t.sol
@@ -7,55 +7,43 @@ import {USDTMock} from "./ERC20Mock.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TalentCommunitySaleTest is Test {
-  TalentCommunitySale talentCommunitySale;
+    TalentCommunitySale talentCommunitySale;
 
-  address initialOwner = address(this);
-  IERC20 paymentToken = new USDTMock();
-  address receivingWallet = address(1337);
-  uint256 tokenDecimals = 6;
+    address initialOwner = address(this);
+    IERC20 paymentToken = new USDTMock();
+    address receivingWallet = address(1337);
+    uint256 tokenDecimals = 6;
 
-  function setUp() public {
-    talentCommunitySale = new TalentCommunitySale(
-      initialOwner,
-      address(paymentToken),
-      receivingWallet,
-      tokenDecimals
-    );
-  }
+    function setUp() public {
+        talentCommunitySale =
+            new TalentCommunitySale(initialOwner, address(paymentToken), receivingWallet, tokenDecimals);
+    }
 
-
-
-      function testTier1WithinLimit() public {
+    function testTier1WithinLimit() public {
         //talentCommunitySale.buyTier1(0); // Another 50, reaching limit
         assertEq(talentCommunitySale.tier1Bought(), 0);
     }
 
-  function test_ReceivingWalletReturnsTheWalletWeDeployedWith() public view {
-    address result = talentCommunitySale.receivingWallet();
+    function test_ReceivingWalletReturnsTheWalletWeDeployedWith() public view {
+        address result = talentCommunitySale.receivingWallet();
 
-    assertEq(result, receivingWallet);
-  }
- 
-  function testInitialTotalRaisedIsZero() public {
-    uint256 initialTotalRaised = talentCommunitySale.totalRaised();
-    assertEq(initialTotalRaised, 0);
-  }
+        assertEq(result, receivingWallet);
+    }
 
+    function testInitialTotalRaisedIsZero() public {
+        uint256 initialTotalRaised = talentCommunitySale.totalRaised();
+        assertEq(initialTotalRaised, 0);
+    }
 
-  function testTotalRaisedUpdatesCorrectly() public {
-    uint256 raisedAmount = 0;
-    uint256 updatedTotalRaised = talentCommunitySale.totalRaised();
-    assertEq(updatedTotalRaised, raisedAmount);
-  }
+    function testTotalRaisedUpdatesCorrectly() public {
+        uint256 raisedAmount = 0;
+        uint256 updatedTotalRaised = talentCommunitySale.totalRaised();
+        assertEq(updatedTotalRaised, raisedAmount);
+    }
 
-
-  function testEnableSale() public {
-    assertEq(talentCommunitySale.saleActive(), false);
-    talentCommunitySale.enableSale();
-    assertEq(talentCommunitySale.saleActive(), true);
-  }
-
-
-
-
+    function testEnableSale() public {
+        assertEq(talentCommunitySale.saleActive(), false);
+        talentCommunitySale.enableSale();
+        assertEq(talentCommunitySale.saleActive(), true);
+    }
 }
