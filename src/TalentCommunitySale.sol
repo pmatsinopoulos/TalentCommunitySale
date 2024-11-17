7ebda29ce9335902b7ac6a6bad1e1e7521c02f38
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-17 09:42:47 +0200

    public turned to external

--- src/TalentCommunitySale.sol
+++ src/TalentCommunitySale.sol
@@ -88,7 +88,7 @@ contract TalentCommunitySale {
      * NOTE: Renouncing ownership will leave the contract without an owner,
      * thereby disabling any functionality that is only available to the owner.
      */
-    function renounceOwnership() public {
+    function renounceOwnership() external {
         onlyOwner();
         _transferOwnership(address(0));
     }
@@ -97,7 +97,7 @@ contract TalentCommunitySale {
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
      */
-    function transferOwnership(address newOwner) public {
+    function transferOwnership(address newOwner) external {
         onlyOwner();
         if (newOwner == address(0)) {
             revert OwnableInvalidOwner(address(0));
