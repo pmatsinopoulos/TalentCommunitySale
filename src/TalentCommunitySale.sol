d5461d41f64e0b937b85a8da67685d83c8a10223
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-18 22:17:59 +0200

    Removed console from contract

--- src/TalentCommunitySale.sol
+++ src/TalentCommunitySale.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {console} from "forge-std/Test.sol";
 
 contract TalentCommunitySale {
     uint256 private tokenDecimals;
