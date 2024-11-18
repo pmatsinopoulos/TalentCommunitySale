d41f8da70482bc8771ebddd7caa76a00405c4ac6
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-19 00:26:15 +0200

    Scripts to deploy

---
+++ script/DeployTalentCommunitySale.s.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {Script, console} from "forge-std/Script.sol";
+import {TalentCommunitySale} from "../src/TalentCommunitySale.sol";
+import {USDTMock} from "./ERC20Mock.sol";
+
+contract DeployUSDTMock is Script {
+    function run() public {
+      vm.startBroadcast();
+
+      new USDTMock();
+
+      vm.stopBroadcast();
+    }
+}
+
+contract DeployTalentCommunitySale is Script {
+    function setUp() public {
+
+    }
+
+    function run(
+      address initialOwner,
+      address _paymentToken,
+      address _receivingWallet,
+      uint256 _tokenDecimals
+    ) public {
+      vm.startBroadcast();
+
+      new TalentCommunitySale(
+        initialOwner,
+        _paymentToken,
+        _receivingWallet,
+        _tokenDecimals
+      );
+
+      vm.stopBroadcast();
+    }
+
+    function enableSale(
+      address talentCommunitySaleAddress
+    ) public {
+
+      vm.startBroadcast();
+
+      TalentCommunitySale talentCommunitySale = TalentCommunitySale(talentCommunitySaleAddress);
+
+      talentCommunitySale.enableSale();
+
+      vm.stopBroadcast();
+    }
+}
